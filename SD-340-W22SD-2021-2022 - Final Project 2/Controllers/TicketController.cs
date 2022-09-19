12892d4cfd36ae8d4f69eeac985630b075de07da
Dane Dwarka <danedwarka@hotmail.com>
2022-09-19 01:17:08 -0500

    Add Binding for Create Action of Ticket Controller

--- SD-340-W22SD-2021-2022 - Final Project 2/Controllers/TicketController.cs
+++ SD-340-W22SD-2021-2022 - Final Project 2/Controllers/TicketController.cs
@@ -26,17 +26,19 @@ namespace SD_340_W22SD_2021_2022___Final_Project_2.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(int projectId, string name, int hours, Priority priority)
+        //public IActionResult Create(int projectId, string name, int hours, Priority priority)
+        public IActionResult Create(int projectId, 
+            [Bind("Completed, Name, Hours, Priority, ProjectId")] Ticket ticket)
         {
-            Ticket ticket = new Ticket();
-            ticket.ProjectId = projectId;
-            ticket.Name = name;
-            ticket.Hours = hours;
-            ticket.Priority = priority;
-            ticket.Completed = false;
-
-            _context.Ticket.Add(ticket);
-            _context.SaveChanges();
+            if (ModelState.IsValid)
+            {
+                _context.Ticket.Add(ticket);
+                _context.SaveChanges();
+            }
+            else
+            {
+                return BadRequest();
+            }
 
             return RedirectToAction("Details", "Project", new { projectId = projectId });
         }
