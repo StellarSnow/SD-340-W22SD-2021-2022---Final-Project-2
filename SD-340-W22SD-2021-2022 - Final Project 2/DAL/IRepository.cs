a1cbd25885b209a4bf322beeb8f4f8fab182a9cf
MaxWiens28 <mjwiens@yahoo.com>
2022-10-11 16:00:12 -0500

    Created generic Repo to base all repository on

---
+++ SD-340-W22SD-2021-2022 - Final Project 2/DAL/IRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace SD_340_W22SD_2021_2022___Final_Project_2.DAL
+{
+
+    public interface IRepository<T> where T : class
+    {
+        void Add(T entity);
+
+        T Get(int id);
+        T Get(Func<T, bool> predicate);
+        ICollection<T> GetAll();
+        ICollection<T> GetList(Func<T, bool> predicate);
+        T Update(T entity);
+
+        void Delete(T entity);
+
+        void Save();
+    }
+}
