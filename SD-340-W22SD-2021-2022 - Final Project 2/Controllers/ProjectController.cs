f5d673b0959de2feb97985abe7b28dd4a5f9252b
MaxWiens28 <mjwiens@yahoo.com>
2022-10-11 10:48:05 -0500

    initial comment

--- SD-340-W22SD-2021-2022 - Final Project 2/Controllers/ProjectController.cs
+++ SD-340-W22SD-2021-2022 - Final Project 2/Controllers/ProjectController.cs
@@ -8,6 +8,7 @@ using SD_340_W22SD_2021_2022___Final_Project_2.Models.ViewModels;
 
 namespace SD_340_W22SD_2021_2022___Final_Project_2.Controllers
 {
+    // Hello
     public class ProjectController : Controller
     {
         private ApplicationDbContext _context;
