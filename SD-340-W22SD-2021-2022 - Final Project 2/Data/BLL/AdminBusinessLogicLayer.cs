d50bda68ee27123ffc8f47468e48e9941ca42e09
Sushant Sharma <sushantsharmapb13@gmail.com>
2022-10-11 22:20:16 -0500

    Refactored code.

---
+++ SD-340-W22SD-2021-2022 - Final Project 2/Data/BLL/AdminBusinessLogicLayer.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SD_340_W22SD_2021_2022___Final_Project_2.Models;
+
+namespace SD_340_W22SD_2021_2022___Final_Project_2.Data.BLL
+{
+    public class AdminBusinessLogicLayer
+    {
+        public ApplicationDbContext _context { get; set; }
+
+        public AdminBusinessLogicLayer(ApplicationDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        public List<ApplicationUser> GetUnassignedDevelopers()
+        {
+            List<string> userIds = _context.UserRoles.Select(ur => ur.UserId).ToList();
+            List<ApplicationUser> users = _context.Users.Where(u => !userIds.Contains(u.Id)).ToList();
+            
+            return users;
+        }
+
+
+
+    }
+}
