03db4ccc3f424477c7c94d6ee916029484849742
Shaun Senow <shaunsenow@gmail.com>
2022-10-13 16:32:37 -0500

    Updated CommentTask

--- SD-340-W22SD-2021-2022 - Final Project 2/Controllers/CommentController.cs
+++ SD-340-W22SD-2021-2022 - Final Project 2/Controllers/CommentController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SD_340_W22SD_2021_2022___Final_Project_2.Data;
+using SD_340_W22SD_2021_2022___Final_Project_2.Data.BLL;
+using SD_340_W22SD_2021_2022___Final_Project_2.Data.DAL;
 using SD_340_W22SD_2021_2022___Final_Project_2.Models;
 using SD_340_W22SD_2021_2022___Final_Project_2.Models.ViewModels;
 
@@ -10,12 +12,18 @@ namespace SD_340_W22SD_2021_2022___Final_Project_2.Controllers
 {
     public class CommentController : Controller
     {
+
+        private readonly TicketRepository _ticketRepository;
+        private TicketBusinessLogic _ticketBLL;
         private ApplicationDbContext _context;
+        private ProjectRepository _projectRepository;
         private readonly UserManager<ApplicationUser> _userManager;
 
         public CommentController(ApplicationDbContext context,
             UserManager<ApplicationUser> userManager)
         {
+            _ticketRepository = new TicketRepository(context);
+            _ticketBLL = new TicketBusinessLogic(_ticketRepository, _projectRepository, userManager);
             _context = context;
             _userManager = userManager;
         }
@@ -29,10 +37,7 @@ namespace SD_340_W22SD_2021_2022___Final_Project_2.Controllers
 
             try
             {
-                comments = _context.Comment
-                    .Include(u => u.User)
-                    .Where(c => c.TicketId == ticketId)
-                    .ToList();
+                comments = (List<Comment>?)_ticketBLL.CommentsOnTask(ticketId);
             }
             catch (Exception ex)
             {
