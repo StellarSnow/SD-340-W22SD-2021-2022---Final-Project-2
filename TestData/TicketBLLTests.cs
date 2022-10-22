3202df5ef944020c78f1fcb64b5090b3d462435f
Sushant Sharma <sushantsharmapb13@gmail.com>
2022-10-21 19:03:42 -0500

    Added remaining tests for TicketBLL methods.

--- TestData/TicketBLLTests.cs
+++ TestData/TicketBLLTests.cs
@@ -6,6 +6,7 @@ using SD_340_W22SD_2021_2022___Final_Project_2.Data.BLL;
 using SD_340_W22SD_2021_2022___Final_Project_2.Data.DAL;
 using SD_340_W22SD_2021_2022___Final_Project_2.Models;
 using SD_340_W22SD_2021_2022___Final_Project_2.Models.ViewModels;
+using System.Security.Claims;
 using static TestData.DeveloperTests;
 
 namespace TestData
@@ -19,8 +20,8 @@ namespace TestData
         public static ApplicationDbContext _dbContext { get; set; }
 
 
-        [ClassInitialize]
-        public static void InitializeClass(TestContext testContext)
+        [TestInitialize]
+        public void InitializeTest()
         {
             DbContextOptions<ApplicationDbContext> options = new DbContextOptionsBuilder<ApplicationDbContext>()
                                 .UseInMemoryDatabase(databaseName: "TestDatabase")
@@ -94,6 +95,15 @@ namespace TestData
             );
         }
 
+        [TestCleanup]
+        public void CleanUpTests()
+        {
+            _dbContext.Database.EnsureDeleted();
+            _users = null;
+            _ticketBLL = null;
+            _userManager = null;
+        }
+
         [DataRow(null, 1, new string[] { }, Priority.high)]
         [TestMethod]
         public async Task TestCreateTicketFailure(Ticket ticket, int projectId, string[] taskOwnerIds, Priority priority)
@@ -106,14 +116,6 @@ namespace TestData
         [TestMethod]
         public async Task TestCreateTicketSuccess()
         {
-            var pm = await _userManager.FindByIdAsync(_users[0].Id);
-            var dev1 = await _userManager.FindByIdAsync(_users[1].Id);
-            var dev2 = await _userManager.FindByIdAsync(_users[2].Id);
-
-            var pmRoles = _userManager.GetRolesAsync(pm);
-            var dev1Roles = _userManager.GetRolesAsync(dev1);
-            var dev2Roles = _userManager.GetRolesAsync(dev2);
-
             Ticket ticketInputModel = new Ticket()
             {
                 Id = 1,
@@ -131,6 +133,297 @@ namespace TestData
             CreateTicketBLLViewModel result = await _ticketBLL.CreateTicket(ticketInputModel, projectId, taskOwnerIds, Priority.high);
 
             Assert.IsTrue(result.Succeeded);
+        }
+
+        [DataRow(1, 1)]
+        [TestMethod]
+        public async Task TestAddToWatchListAsyncFailure(int projectId, int ticketId)
+        {
+            Ticket ticketInputModel = new Ticket()
+            {
+                Id = ticketId,
+                Name = "Test Ticket 1",
+                ProjectId = projectId,
+                Completed = false,
+                Hours = 2,
+                Priority = Priority.high
+            };
+
+            // user at index 1 is assigned to the project 1
+            string[] taskOwnerIds = new string[] { _users[1].Id };
+
+            CreateTicketBLLViewModel createTicket = await _ticketBLL.CreateTicket(ticketInputModel, projectId, taskOwnerIds, Priority.high);
+
+            Assert.IsTrue(createTicket.Succeeded);
+
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, _users[2].UserName),
+                new Claim(ClaimTypes.NameIdentifier, _users[2].Id),
+                new Claim(ClaimTypes.Email, _users[2].Email)
+            };
+            
+            ClaimsIdentity identity = new ClaimsIdentity(claims, "Mock");
+            ClaimsPrincipal user = new ClaimsPrincipal(identity);
+
+            Assert.IsTrue(user.Identity.IsAuthenticated);
+
+            AddToWatchListBLLViewModel addToWatchList = await _ticketBLL.AddToWatchListAsync(user, projectId, ticketId);
+
+            Assert.IsTrue(addToWatchList.Unauthorized);
+        }
+
+        [DataRow(1, 1)]
+        [TestMethod]
+        public async Task TestAddToWatchListAsyncSuccess(int projectId, int ticketId)
+        {
+            Project project = await _dbContext.Project.FirstOrDefaultAsync(p => p.Id == projectId);
+
+            Assert.IsNotNull(project);
+
+            Ticket ticketInputModel = new Ticket()
+            {
+                Id = ticketId,
+                Name = "Test Ticket 1",
+                ProjectId = projectId,
+                Completed = false,
+                Hours = 2,
+                Priority = Priority.high
+            };
+
+            // user at index 1 is assigned to the project 1
+            string[] taskOwnerIds = new string[] { _users[1].Id };
+
+            CreateTicketBLLViewModel createTicket = await _ticketBLL.CreateTicket(ticketInputModel, projectId, taskOwnerIds, Priority.high);
+
+            Assert.IsTrue(createTicket.Succeeded);
+
+            ApplicationUser userToBeAddedToWatchList = await _userManager.FindByIdAsync(_users[2].Id);
+
+            Assert.IsNotNull(userToBeAddedToWatchList);
+
+            project.Developers.Add(userToBeAddedToWatchList);
+
+            userToBeAddedToWatchList.Projects.Add(project);
+
+            await _dbContext.SaveChangesAsync();
+
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, userToBeAddedToWatchList.UserName),
+                new Claim(ClaimTypes.NameIdentifier, userToBeAddedToWatchList.Id),
+                new Claim(ClaimTypes.Email, userToBeAddedToWatchList.Email)
+            };
+
+            ClaimsIdentity identity = new ClaimsIdentity(claims, "Mock");
+            ClaimsPrincipal userClaimsPrincipal = new ClaimsPrincipal(identity);
+
+            Assert.IsTrue(userClaimsPrincipal.Identity.IsAuthenticated);
+
+            AddToWatchListBLLViewModel addToWatchList = await _ticketBLL.AddToWatchListAsync(userClaimsPrincipal, projectId, ticketId);
+
+            Assert.IsTrue(addToWatchList.Succeeded);
+        }
+
+        [DataRow(1, 1)]
+        [TestMethod]
+        public async Task TestToggleTicketAsyncFailure(int projectId, int ticketId)
+        {
+            Ticket ticketInputModel = new Ticket()
+            {
+                Id = ticketId,
+                Name = "Test Ticket 1",
+                ProjectId = projectId,
+                Completed = false,
+                Hours = 2,
+                Priority = Priority.high
+            };
+
+            // user at index 1 is assigned to the project 1
+            string[] taskOwnerIds = new string[] { _users[1].Id };
+
+            CreateTicketBLLViewModel createTicket = await _ticketBLL.CreateTicket(ticketInputModel, projectId, taskOwnerIds, Priority.high);
+
+            Assert.IsTrue(createTicket.Succeeded);
+
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, _users[2].UserName),
+                new Claim(ClaimTypes.NameIdentifier, _users[2].Id),
+                new Claim(ClaimTypes.Email, _users[2].Email)
+            };
+
+            ClaimsIdentity identity = new ClaimsIdentity(claims, "Mock");
+            ClaimsPrincipal userClaimsPrincipal = new ClaimsPrincipal(identity);
+
+            Assert.IsTrue(userClaimsPrincipal.Identity.IsAuthenticated);
+
+            ToggleTicketBLLViewModel toggleTicket = await _ticketBLL.ToggleTicketAsync(userClaimsPrincipal, ticketId);
+
+            Assert.IsTrue(toggleTicket.Unauthorized);
+        }
+
+        [DataRow(1, 1)]
+        [TestMethod]
+        public async Task TestToggleTicketAsyncSuccess(int projectId, int ticketId)
+        {
+            Ticket ticketInputModel = new Ticket()
+            {
+                Id = ticketId,
+                Name = "Test Ticket 1",
+                ProjectId = projectId,
+                Completed = false,
+                Hours = 2,
+                Priority = Priority.high
+            };
+
+            // user at index 1 is assigned to the project 1
+            string[] taskOwnerIds = new string[] { _users[1].Id };
+
+            CreateTicketBLLViewModel createTicket = await _ticketBLL.CreateTicket(ticketInputModel, projectId, taskOwnerIds, Priority.high);
+
+            Assert.IsTrue(createTicket.Succeeded);
+
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, _users[1].UserName),
+                new Claim(ClaimTypes.NameIdentifier, _users[1].Id),
+                new Claim(ClaimTypes.Email, _users[1].Email)
+            };
+
+            ClaimsIdentity identity = new ClaimsIdentity(claims, "Mock");
+            ClaimsPrincipal userClaimsPrincipal = new ClaimsPrincipal(identity);
+
+            Assert.IsTrue(userClaimsPrincipal.Identity.IsAuthenticated);
+
+            ToggleTicketBLLViewModel toggleTicket = await _ticketBLL.ToggleTicketAsync(userClaimsPrincipal, ticketId);
+
+            Assert.IsTrue(toggleTicket.Succeeded);
+        }
+
+        [DataRow(1, 1, 1)]
+        [TestMethod]
+        public async Task TestChangeRequiredHoursAsyncFailure(int projectId, int ticketId, int requiredHours)
+        {
+            Ticket ticketInputModel = new Ticket()
+            {
+                Id = ticketId,
+                Name = "Test Ticket 1",
+                ProjectId = projectId,
+                Completed = false,
+                Hours = 2,
+                Priority = Priority.high
+            };
+
+            // user at index 1 is assigned to the project 1
+            string[] taskOwnerIds = new string[] { _users[1].Id };
+
+            CreateTicketBLLViewModel createTicket = await _ticketBLL.CreateTicket(ticketInputModel, projectId, taskOwnerIds, Priority.high);
+
+            Assert.IsTrue(createTicket.Succeeded);
+
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, _users[2].UserName),
+                new Claim(ClaimTypes.NameIdentifier, _users[2].Id),
+                new Claim(ClaimTypes.Email, _users[2].Email)
+            };
+
+            ClaimsIdentity identity = new ClaimsIdentity(claims, "Mock");
+            ClaimsPrincipal userClaimsPrincipal = new ClaimsPrincipal(identity);
+
+            Assert.IsTrue(userClaimsPrincipal.Identity.IsAuthenticated);
+
+            ChangeRequiredHoursAsyncBLLViewModel changeRequiredHours = await _ticketBLL.ChangeRequiredHoursAsync(userClaimsPrincipal, ticketId, requiredHours);
+
+            Assert.IsTrue(changeRequiredHours.Unauthorized);
+        }
+
+        [DataRow(1, 1, 1)]
+        [TestMethod]
+        public async Task TestChangeRequiredHoursAsyncSuccess(int projectId, int ticketId, int requiredHours)
+        {
+            Ticket ticketInputModel = new Ticket()
+            {
+                Id = ticketId,
+                Name = "Test Ticket 1",
+                ProjectId = projectId,
+                Completed = false,
+                Hours = 2,
+                Priority = Priority.high
+            };
+
+            // user at index 1 is assigned to the project 1
+            string[] taskOwnerIds = new string[] { _users[1].Id };
+
+            CreateTicketBLLViewModel createTicket = await _ticketBLL.CreateTicket(ticketInputModel, projectId, taskOwnerIds, Priority.high);
+
+            Assert.IsTrue(createTicket.Succeeded);
+
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, _users[1].UserName),
+                new Claim(ClaimTypes.NameIdentifier, _users[1].Id),
+                new Claim(ClaimTypes.Email, _users[1].Email)
+            };
+
+            ClaimsIdentity identity = new ClaimsIdentity(claims, "Mock");
+            ClaimsPrincipal userClaimsPrincipal = new ClaimsPrincipal(identity);
+
+            Assert.IsTrue(userClaimsPrincipal.Identity.IsAuthenticated);
+
+            ChangeRequiredHoursAsyncBLLViewModel changeRequiredHours = await _ticketBLL.ChangeRequiredHoursAsync(userClaimsPrincipal, ticketId, requiredHours);
+
+            Assert.IsTrue(changeRequiredHours.Succeeded);
+        }
+
+        [DataRow(1, 1)]
+        [TestMethod]
+        public async Task TestCommentsOnTaskSuccess(int projectId, int ticketId)
+        {
+            Ticket ticketInputModel = new Ticket()
+            {
+                Id = ticketId,
+                Name = "Test Ticket 1",
+                ProjectId = projectId,
+                Completed = false,
+                Hours = 2,
+                Priority = Priority.high
+            };
+
+            // user at index 1 is assigned to the project 1
+            string[] taskOwnerIds = new string[] { _users[1].Id };
+
+            CreateTicketBLLViewModel createTicket = await _ticketBLL.CreateTicket(ticketInputModel, projectId, taskOwnerIds, Priority.high);
+
+            Assert.IsTrue(createTicket.Succeeded);
+
+            Ticket ticket = await _dbContext.Ticket.FirstOrDefaultAsync(t => t.Id == ticketId);
+
+            Assert.IsNotNull(ticket);
+
+            Comment comment = new Comment()
+            {
+                Id = 1,
+                Content = "Test comment",
+                TicketId = ticketId,
+                UserId = _users[1].Id
+            };
+
+
+            _dbContext.Comment.Add(comment);
+            ticket.Comment.Add(comment);
+
+            await _dbContext.SaveChangesAsync();
+
+            ICollection<Comment> comments = _ticketBLL.CommentsOnTask(ticketId);
+
+            Assert.IsNotNull(comments);
+            Assert.AreEqual(comments.Count, 1);
+            Assert.AreEqual(comments.First().Id, comment.Id);
+            Assert.AreEqual(comments.First().Content, comment.Content);
+            Assert.AreEqual(comments.First().TicketId, comment.TicketId);
+            Assert.AreEqual(comments.First().UserId, comment.UserId);
         }
     }
 }
